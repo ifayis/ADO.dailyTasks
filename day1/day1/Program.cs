96a536ebf12b8b2ebe9bfbbe1b32f3dd37d52fbe
Fayis KV <mohammedfayis135@gmail.com>
2025-12-03 12:15:29 +0530

    Add project files.

---
+++ day1/day1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            managestudent details = new managestudent();
+            details.getdetails();
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ day1/day1/managestudent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1
+{
+        public class managestudent
+        {
+        private string conn = "server=localhost\\SQLEXPRESS;database=studentsDB;trusted_connection=true";
+
+        public void getdetails()
+        {
+            string query = @"
+                select s.id, s.name, d.department, d.mark
+                from students s
+                inner join studentdetails d
+                on s.detailsid = d.detailsid";
+
+            using (SqlConnection connect = new SqlConnection(conn))
+            {
+                SqlCommand cmd = new SqlCommand(query, connect);
+                connect.Open();
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Console.WriteLine(
+                        $"id : {dr["id"]}" +
+                        $"name : {dr["name"]}" +
+                        $"department : {dr["department"]}" +
+                        $"mark : {dr["mark"]}"
+                        );
+                }
+            }
+        }
+        }
+}

a2b7dd93d8c76899e5795fae1383f96ce724ba4d
Fayis KV <mohammedfayis135@gmail.com>
2025-12-06 17:48:59 +0530

    ADO

--- day3/day3/storedprocedure.cs
+++ day3/day3/storedprocedure.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,34 @@ namespace day3
 {
     internal class storedprocedure
     {
+        String connection = @"server=localhost\SQLEXPRESS;database=StudentsDB;trusted_connection=true";
+        
+        public void getstudentdetails(int id)
+        {
+            using (SqlConnection con = new SqlConnection(connection)) 
+            using (SqlCommand cmd = new SqlCommand("getallstudents", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("id", id);
+
+                con.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    if (dr.Read())
+                    {
+                        Console.WriteLine(
+                            $"id{ dr["id"]}" +
+                            $"name{ dr["name"]}" +
+                            $"age { dr["age"]}"
+                            );
+                    }
+                    else
+                    {
+                        Console.WriteLine("data not available with the id" + id);
+                    }
+                }
+            } 
+        }
     }
 }

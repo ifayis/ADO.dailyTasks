130eb1462154d9f59640be0b36b4a3871d462f6f
Fayis KV <mohammedfayis135@gmail.com>
2025-12-09 17:48:48 +0530

    ADO

---
+++ trial1/trial1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trial1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sp run = new sp();
+            run.main();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ trial1/trial1/sp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trial1
+{
+    internal class sp
+    {
+        public void main()
+        {
+            String connection = @"server=localhost\SQLEXPRESS;database=newstudentsDB;trusted_connection=true";
+            DataSet ds = new DataSet();
+
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                using (SqlCommand cmd = new SqlCommand("viewstudents", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id",1);
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        da.Fill(ds);
+                    }
+                }
+            }
+            DataTable dt = ds.Tables[0];
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine($"id : {row["id"]} - name : {row["name"]} - age : {row["age"]}");
+            }
+        }
+    }
+}

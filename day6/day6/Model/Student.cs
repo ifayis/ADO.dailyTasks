607c0a6c62b9a70cef170c6b516c8d5106aa92d6
Fayis KV <mohammedfayis135@gmail.com>
2025-12-17 14:24:58 +0530

    ADO

--- day6/day6/Model/Student.cs
+++ day6/day6/Model/Student.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace day6.Models
 {
+    [Table("Student")]
     public class Student
     {
         public int Id { get; set; }
